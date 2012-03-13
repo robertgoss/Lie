f5283319f90480dcb744c4aaf944e26c6959303a
Robert Goss <goss.robert@gmail.com>
2012-03-13 23:32:56 +0000

    Enrich ratio with algebraic properties

--- Ratio.hs
+++ Ratio.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultiParamTypeClasses,
+             FlexibleInstances     #-}
 module Ratio where
 
 
@@ -6,15 +8,49 @@ import Data.Ratio
 import Numeric.Algebra
 
 import qualified Prelude as P
+import Prelude hiding ((+),(*),negate)
 
 --Make Ratio a semi ring in the natural way
 
-instance (Additive r,P.Integral r) => Additive (Ratio r) where
-    (+) a b = a+b
+instance (Integral a) => Additive (Ratio a) where
+    (+) x y = (x1 P.* y2 P.+ y1 P.* x2)%(x2 P.* y2)
+        where x1 = numerator x
+              x2 = denominator x
+              y1 = numerator y
+              y2 = denominator y
 
-instance (Additive r,P.Integral r) => Abelian (Ratio r)
+instance (Integral a) => Abelian (Ratio a)
 
-instance (Additive r,P.Integral r) => Multiplicative (Ratio r) where
-    (*) a b = a*b
+instance  (Integral a) => Multiplicative (Ratio a) where
+    (*) x y = (x1 P.* y1)%(x2 P.* y2)
+        where x1 = numerator x
+              x2 = denominator x
+              y1 = numerator y
+              y2 = denominator y
 
-instance (Semiring a,P.Integral a) => Semiring (Ratio a)
+instance (Integral a) => Semiring (Ratio a)
+
+instance (Integral a) => Monoidal (Ratio a) where
+    zero = 0%1
+
+instance  (Integral a,Integral b,Semiring b) => LeftModule b (Ratio a) where
+    (.*) n d = prod%b
+        where a = numerator d
+              b = denominator d
+              prod = P.sum $ replicate (fromIntegral n) a
+
+instance (Integral a,Integral b,Semiring b) => RightModule b (Ratio a) where
+    (*.) d n = n.*d
+
+instance (Integral r) => Group (Ratio r) where
+    negate d = (P.negate a)%b
+        where  a = numerator d
+               b = denominator d
+
+instance (Integral r) => Unital (Ratio r) where
+    one = 1%1
+
+instance (Integral r) => Division (Ratio r) where
+    recip d = b%a
+        where a = numerator d
+              b = denominator d
